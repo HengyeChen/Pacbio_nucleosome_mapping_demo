0bdb7977b4a997c279ca730cf2cda39e58ce0d87
HengyeChen <hengyechen@gmail.com>
2023-01-21 23:23:58 +0800

    Update rev2fw_header_v2.m

--- rev2fw_header_v2.m
+++ rev2fw_header_v2.m
@@ -1,6 +1,6 @@
 function seqs_c = rev2fw_header_v2(ccs_fq,seqs_rc)
 %%
-%seqs_c contains raw reads and reverse complimentary reads
+%seqs_c contains raw reads and reverse complementary reads
 %ccs_fq is the input file which should be a fastq file
 %seqs_rc is the output file
 
@@ -9,7 +9,7 @@ tic
 %% generate a new file containing reads and reversed reads
 [~,len] = size(seqs);
 seqs_c = cell(len*2,2);% create a new cell from headers and sequences
-for i = 1:len%input headers, seqs, and the reverse complimentary seqs to seqs_c
+for i = 1:len%input headers, seqs, and the reverse complementary seqs to seqs_c
     seq = seqs{1,i};% get one read from the seqs
     seqs_c{i,2} = header{1,i};% set the second column of seqs_c as headers
     seqs_c{len+i,2} = header{1,i};
